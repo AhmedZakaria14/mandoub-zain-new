12bf099564f16788a6106a884b12ea2da3d74555
AhmedZakaria14 <azeco77@gmail.com>
2026-08-31 15:19:53 +0300

    Remove broken slideshow images

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -6,13 +6,9 @@ import { BRAND_IMAGE_PATH } from '@/lib/config';
 const homeImages = [
   { src: BRAND_IMAGE_PATH, alt: "مندوب شركة زين الرياض اتصل 0534768368 لتركيب وتأسيس 5G والألياف" },
   { src: "/images/slideshow/1_s3knrl.webp", alt: "راوتر زين 5G المنزلي باللون الأبيض مع إنترنت سريع" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461697/zain_fiber_rdqmd6.jpg", alt: "تركيب بوكسات ألياف بصرية زين فايبر بسرعة واحترافية" },
   { src: "/images/slideshow/2_ivbo98.webp", alt: "زين تتصدر سرعة الإنترنت الثابت وتغطية 5G في المملكة" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461698/WhatsApp_Image_2026-04-28_at_2.57.08_PM_z4ytos.jpg", alt: "تأسيس وإنشاء شبكات الألياف البصرية وزين فايبر للمنازل" },
   { src: "/images/slideshow/3_d2uvsg.webp", alt: "شبكة زين 5G الأفضل في الرياض بأقوى تغطية وأعلى سرعة" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461697/WhatsApp_Image_2026-04-28_at_2.57.08_PM_2_ztdosg.jpg", alt: "تمديد كابلات وتجهيز بوكسات الألياف البصرية زين بأعلى معايير الجودة" },
   { src: "/images/slideshow/4_mve8v4.webp", alt: "راوتر وشريحة زين 5G للإنترنت المنزلي سريع التأسيس" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461697/WhatsApp_Image_2026-04-28_at_2.57.08_PM_1_luo5ux.jpg", alt: "مندوب زين الفني لتركيب الألياف البصرية بالموقع بأسرع وقت" },
   { src: "/images/slideshow/5_l2sdhh.webp", alt: "باقات زين Speed Net 5G غير المحدودة للمنزل" }
 ];
 
