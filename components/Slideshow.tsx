94cf91e5c9162fcef223337c59f8bffd6fa914e1
AhmedZakaria14 <azeco77@gmail.com>
2026-08-31 15:13:37 +0300

    Replace broken slideshow images

--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -5,15 +5,15 @@ import { BRAND_IMAGE_PATH } from '@/lib/config';
 
 const homeImages = [
   { src: BRAND_IMAGE_PATH, alt: "مندوب شركة زين الرياض اتصل 0534768368 لتركيب وتأسيس 5G والألياف" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777279049/1_s3knrl.png", alt: "عروض وسرعات إنترنت زين 5G الهائلة للمنزل لتجربة ألعاب وبث بلا تقطيع" },
+  { src: "/images/slideshow/1_s3knrl.webp", alt: "راوتر زين 5G المنزلي باللون الأبيض مع إنترنت سريع" },
   { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461697/zain_fiber_rdqmd6.jpg", alt: "تركيب بوكسات ألياف بصرية زين فايبر بسرعة واحترافية" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777279051/2_ivbo98.png", alt: "باقات زين المفتوحة واللامحدودة للإنترنت المنزلي والألياف البصرية المتطورة" },
+  { src: "/images/slideshow/2_ivbo98.webp", alt: "زين تتصدر سرعة الإنترنت الثابت وتغطية 5G في المملكة" },
   { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461698/WhatsApp_Image_2026-04-28_at_2.57.08_PM_z4ytos.jpg", alt: "تأسيس وإنشاء شبكات الألياف البصرية وزين فايبر للمنازل" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777279053/3_d2uvsg.png", alt: "أفضل تغطية إنترنت منزلي من شركة زين في الرياض والمملكة كاملة" },
+  { src: "/images/slideshow/3_d2uvsg.webp", alt: "شبكة زين 5G الأفضل في الرياض بأقوى تغطية وأعلى سرعة" },
   { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461697/WhatsApp_Image_2026-04-28_at_2.57.08_PM_2_ztdosg.jpg", alt: "تمديد كابلات وتجهيز بوكسات الألياف البصرية زين بأعلى معايير الجودة" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777279052/4_mve8v4.png", alt: "تأسيس وتركيب إنترنت 5G سريع ومجاني مع راوتر حديث" },
+  { src: "/images/slideshow/4_mve8v4.webp", alt: "راوتر وشريحة زين 5G للإنترنت المنزلي سريع التأسيس" },
   { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777461697/WhatsApp_Image_2026-04-28_at_2.57.08_PM_1_luo5ux.jpg", alt: "مندوب زين الفني لتركيب الألياف البصرية بالموقع بأسرع وقت" },
-  { src: "https://res.cloudinary.com/dxvjqrb9l/image/upload/v1777279050/5_l2sdhh.png", alt: "موظف مبيعات زين لخدمات الإنترنت المنزلي ورفع طلبات التأسيس فوراً" }
+  { src: "/images/slideshow/5_l2sdhh.webp", alt: "باقات زين Speed Net 5G غير المحدودة للمنزل" }
 ];
 
 export function Slideshow() {
