dc82f5a2f2721da7420efd08acbfa01b2847edf9
AhmedZakaria14 <azeco77@gmail.com>
2026-09-06 18:31:28 +0300

    Update contact phone in fix_page.js

--- fix_page.js
+++ fix_page.js
@@ -130,7 +130,7 @@ export default async function BlogPostPage({ params }: { params: Promise<{ id: s
                 "name": "زين السعودية - تغطية وعروض الفايبر و 5G",
                 "logo": {
                   "@type": "ImageObject",
-                  "url": \`https://www.zain5grouter.com/images/zain-representative-0534768368.webp\`
+                  "url": \`https://www.zain5grouter.com/images/zain-representative-+201010742430.webp\`
                 }
               },
               "datePublished": defaultDate,
