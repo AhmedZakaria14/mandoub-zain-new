c49f7d84f2da9cb00661089cba3ae6f2bbc48a6a
AhmedZakaria14 <azeco77@gmail.com>
2026-09-06 18:31:00 +0300

    Update contact phone in app/blog/page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,8 +5,8 @@ import Image from 'next/image';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'المدونة والمقالات | زين فايبر و5G | 0534768368',
-  description: 'تصفح أحدث المقالات وعروض زين فايبر وإنترنت الجيل الخامس في السعودية، وللاشتراك اتصل بمندوب زين على 0534768368.',
+  title: 'المدونة والمقالات | زين فايبر و5G | +201010742430',
+  description: 'تصفح أحدث المقالات وعروض زين فايبر وإنترنت الجيل الخامس في السعودية، وللاشتراك اتصل بمندوب زين على +201010742430.',
 };
 
 export default function BlogIndexPage() {
