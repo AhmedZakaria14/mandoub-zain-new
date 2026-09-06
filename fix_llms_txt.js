cd8b72869c60684ef2eb2eca7b23014f89677af3
AhmedZakaria14 <azeco77@gmail.com>
2026-09-06 18:31:26 +0300

    Update contact phone in fix_llms_txt.js

--- fix_llms_txt.js
+++ fix_llms_txt.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 if (fs.existsSync('public/llms.txt')) {
   let content = fs.readFileSync('public/llms.txt', 'utf8');
-  content = content.replace(/0534768368|0534768368/g, '0534768368');
+  content = content.replace(/+201010742430|+201010742430/g, '+201010742430');
   fs.writeFileSync('public/llms.txt', content, 'utf8');
   console.log('Updated public/llms.txt');
 }
