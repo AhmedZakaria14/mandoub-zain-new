5dfe9e84d7d486126c7d6f121486c894a31cf7a7
AhmedZakaria14 <azeco77@gmail.com>
2026-09-06 18:35:36 +0300

    Fix contact phone formatting in lib/config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 export const SITE_URL = "https://www.zain5grouter.com";
 export const PHONE_NUMBER = "+201010742430";
-export const WHATSAPP_NUMBER = "966+201010742430";
-export const BRAND_IMAGE_PATH = "/images/zain-representative-+201010742430.webp";
+export const WHATSAPP_NUMBER = "201010742430";
+export const BRAND_IMAGE_PATH = "/images/zain-representative-0534768368.webp";
 export const BRAND_IMAGE_URL = `${SITE_URL}${BRAND_IMAGE_PATH}`;
 export const LOGO_IMAGE_PATH = "/images/zain-5g-logo.webp";
 export const LOGO_IMAGE_URL = `${SITE_URL}${LOGO_IMAGE_PATH}`;
