29dd23bc85ff3cc31eab741a11fbef733252759a
AhmedZakaria14 <azeco77@gmail.com>
2026-09-06 18:32:05 +0300

    Update contact phone in restore_articles.js

--- restore_articles.js
+++ restore_articles.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const OPERATOR = "زين";
-const PHONE = "0534768368"; // Wait, user wants 0534768368 now! In search_results.txt it was 0534768368.
+const PHONE = "+201010742430"; // Wait, user wants +201010742430 now! In search_results.txt it was +201010742430.
 const BRAND = "زين";
 const SITE_NAME = "الدليل السعودي";
 const DOMAIN = "zain5grouter.com";
