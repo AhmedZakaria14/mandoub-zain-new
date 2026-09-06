b46a4f413d9454849bc946a6eae82f8c8ed8acfd
AhmedZakaria14 <azeco77@gmail.com>
2026-09-06 18:35:34 +0300

    Fix contact phone formatting in get_logo.js

--- get_logo.js
+++ get_logo.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-https.get('https://www.zain5grouter.com/images/zain-representative-+201010742430.webp', (res) => {
+https.get('https://www.zain5grouter.com/images/zain-representative-0534768368.webp', (res) => {
   // We can't really parse PNG in plain node easily without a lib.
   // But wait! We can just fetch the CSS of https://sa.zain.com/ar and look for CSS variables.
 });
